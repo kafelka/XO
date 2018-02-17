3c022e75e27983c60b1ea4545524b669f0f2ba49
kafelka <na.hasiok@gmail.com>
2018-02-17 11:36:42 +0000

    Playing with js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const fields = Array.from(document.querySelectorAll(".square"));
-
 let currentPlayer = "X";
 
 fields.forEach(field => field.addEventListener("click", function() {
   if (this.innerHTML === "") {
     this.innerHTML = currentPlayer;
     let fieldCoords = this.dataset.no.split(",");
-    checkIfGameEnded(fieldCoords[0],fieldCoords[1],currentPlayer);
+    console.log(fieldCoords);
+    console.log(fieldCoords[0]);
+    if (checkIfGameEnded(fieldCoords[0], fieldCoords[1], currentPlayer)) {
+     console.log(currentPlayer + " won"); 
+     clearBoard();
+    }
     changePlayer();
   } 
 }));
@@ -15,8 +19,6 @@ let newFields = [];
 while(fields.length) {
   newFields.push(fields.splice(0,3));
 }
-console.log(newFields);
-
 
 function changePlayer() {
   if (currentPlayer === "X") {
@@ -26,16 +28,27 @@ function changePlayer() {
   }
 };
 
-//funkcja ma zwr true jesli symb wygrl gre, false w przeciwienstwie
 function checkIfGameEnded(row, col, symb) {
-  //row 
-  console.log("row");
-  for (let i = 0; i <= 2; i++) {
-    console.log(newFields[row][i].innerHTML === symb);
+  if (newFields[row].every(x => x.innerHTML === symb)) {
+    return true;
+  } 
+  if ([newFields[0][col], newFields[1][col], newFields[2][col]].every(x => x.innerHTML === symb)) {
+    return true;
   }
-  //column
-  console.log("col");
-  for (let i = 0; i <= 2; i++) {
-    console.log(newFields[i][col].innerHTML === symb);
-  }
+  //diagonal missing
+  return false;
 }
+
+function clearBoard() {
+  document.querySelectorAll(".square").forEach(y => y.innerHTML = "");
+  // newFields.forEach(x => x.forEach(y => y.innerHTML = ""));
+}
+
+// function clearBoard() {
+//   for (i = 0; i <= 2; i++) {
+//     for (j = 0; j <= 2; j++) {
+//       newFields[i][j].innerHTML = "";
+//     }
+//   }
+// }
+
